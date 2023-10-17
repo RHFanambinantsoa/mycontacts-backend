963890ecae90d31cd6a4ce79c67b5a24de2d282e
Harilanto <raharisonhfanambinantsoa@gmail.com>
2023-10-17 11:24:07 +0300

    create controllers

---
+++ controllers/contactController.js
@@ -0,0 +1,42 @@
+//@desc get all contacts
+//@route GET /api/contacts
+//@access public
+const getContacts = (req, res) => {
+  res.status(200).json({ message: "get all contact" });
+};
+
+//@desc create contact
+//@route POST /api/contact
+//@access public
+const createContact = (req, res) => {
+  res.status(201).json({ message: "create contact" });
+}; //201 as ressource created
+
+//@desc get one contact
+//@route GET /api/contacts/:id
+//@access public
+const getContact = (req, res) => {
+  res.status(200).json({ message: `get contact for ${req.params.id}` });
+};
+
+//@desc update contact
+//@route PUT /api/contacts
+//@access public
+const updateContact = (req, res) => {
+  res.status(200).json({ message: `update contact for ${req.params.id}` });
+};
+
+//@desc delete contact
+//@route DELETE /api/contacts
+//@access public
+const deleteContact = (req, res) => {
+  res.status(200).json({ message: `delete contact for ${req.params.id}` });
+};
+
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};

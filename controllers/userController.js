a44f66420f0431aebdc3c8cbafd2b65f565afaf1
Nomentsafidy <mnomentsafidy@gmail.com>
2024-02-01 18:13:42 +0300

    test commit jwt kely

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asynchandler = require("express-async-handler");
 const User = require("../models/userModel");
-var passwordHash = require("password-hash");
+const passwordHash = require("password-hash");
+const jwt = require("jsonwebtoken");
 
 //@desc register user
 //@route POST /api/users/register
@@ -12,19 +13,16 @@ const registerUser = asynchandler(async (req, res) => {
     throw new Error("All fields are mandatory");
   }
   const userAvailable = await User.findOne({ email });
-  console.log(userAvailable);
   if (userAvailable) {
     res.status(400);
     throw new Error("User already registered");
   }
   var hashedPassword = passwordHash.generate(password);
-  console.log(hashedPassword);
   const user = await User.create({
     username,
     email,
     password: hashedPassword,
   });
-  console.log(user);
   if (user) {
     res.status(201).json({ _id: user.id, email: user.email });
   } else {
@@ -38,6 +36,33 @@ const registerUser = asynchandler(async (req, res) => {
 //@route POST /api/users/login
 //@access public
 const loginUser = asynchandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const user = await User.findOne({ email });
+  if (user && (await passwordHash.verify(password, user.password))) {
+    console.log("ato");
+    console.log("user:", user);
+    const accessToken = jwt.sign(
+      {
+        //tsy alefa anay payload ny password
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" } //expiration du token
+    );
+
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("email or password is not valid");
+  }
   res.json({ message: "login user" });
 });
 
